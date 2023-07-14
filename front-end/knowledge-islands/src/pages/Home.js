91d5c4a2e416fa2be64a5e0a4233b7d3f55506f5
Otávio Cury <otaviocury.oc@gmail.com>
2023-07-13 21:12:57 -0300

    Ajusting home page.

--- front-end/knowledge-islands/src/pages/Home.js
+++ front-end/knowledge-islands/src/pages/Home.js
@@ -9,8 +9,14 @@ const Home = () => {
     const [error, setError] = useState("");
     const [success, setSuccess] = useState("");
     const [truckFactorProcessState, setTruckFactorProcessState] = useState(0);
+    const [urlValid, setUrlValid] = useState(false);
     const { user } = useContext(AuthContext);
     const url = useRef("");
+
+    const changeUrl = (event) => {
+        const urlRegEx = new RegExp('^(https:\/\/)?(www\.)?(github\.com|gitlab\.com|bitbucket\.org)\/([a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+)(\.git)?$');
+        setUrlValid(urlRegEx.test(event.target.value));
+    };
     const branch = useRef("");
     const createTruckFactorProcess = async(event) => {
         event.preventDefault();
@@ -43,14 +49,15 @@ const Home = () => {
                         <Form>
                         <Form.Group className="mb-3" controlId="formBasicsUrl">
                             <Form.Label>Url Repository</Form.Label>
-                            <Form.Control ref={url} type="text" required/>
+                            <Form.Control ref={url} type="text" required onChange={changeUrl}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicsBranch">
                             <Form.Label>Branch</Form.Label>
                             <Form.Control ref={branch} type="text"/>
                         </Form.Group>
                         <div className="d-grid">
-                        <Button variant="primary" type="submit" onClick={createTruckFactorProcess}>
+                        <Button variant="primary" type="submit" disabled={!urlValid}
+                        onClick={createTruckFactorProcess}>
                             Analyze
                         </Button>
                         </div>
